fa46030ef3a7a9265a3400d8838495a218a213a3
ArturMachado07 <59847055+ArturMachado07@users.noreply.github.com>
2026-08-23 20:34:59 +0100

    Hero: usa banner-hero2.webp

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
           div externo. */}
       <div className="absolute inset-0">
         <MediaSlot
-          baseName="banner-hero1"
+          baseName="banner-hero2"
           alt="Formação Wealth Academy"
           className="h-full w-full"
           priority
